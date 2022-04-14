2932785503b2692b146afb9997f7bde3b97992d2
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-04-14 18:41:06 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+package io.github.JoltMuz.bracket;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        this.getCommand("bracket").setExecutor(new start());
+        getServer().getPluginManager().registerEvents(new tp(), this);
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+    public static String sign = ChatColor.YELLOW + ChatColor.BOLD.toString()+ "Bracket" + ChatColor.DARK_GRAY + " 》 ";
+}
